9e49c51d33e38a104a1ba1939802873996f09b28
Mike Boiko <michaelboiko@gmail.com>
2018-04-14 13:56:22 -0600

    AllClose closes quickfix windows as well as location lists now

--- templates/skeleton.py
+++ templates/skeleton.py
@@ -1,31 +1,24 @@
-# =======================================================================
-# === Description ...: 
-# === Author ........: Mike Boiko
-# =======================================================================
+#!/usr/bin/env python3.6
+'''
+'''
 
 # Imports {{{1
 
-import argparse  # Parse CLI arguments
 import os        # OS functions
 import sys       # System functions
 
-# Arguments {{{1
-
-parser = argparse.ArgumentParser(description='Find/Replace Multiple Occurences')
-
-parser.add_argument(dest='dictFile', action='store',
-                    help='Name of dictionary csv list')
-parser.add_argument(dest='inputFile', action='store',
-                    help='Name of original text file, where Replacements are to occur')
-
-parser.add_argument('-of', '--outputFile', dest='outputFile', action='store',
-                    default='output.txt',
-                    help='Name of xlsx worksheet - default is 1st sheet')
-
-# Parse all arguments
-args = parser.parse_args()
+# Variables {{{1
 
 # Functions {{{1
+def main(): # {{{2
+    'Main script entry point'
 
 # Main {{{1
 
+# Run script if this file is executed directly
+if __name__ == '__main__':
+    main()
+
+# This module is a way to identify ubuntu
+if 'apport_python_hook' not in sys.modules:
+    os.system('pause')
