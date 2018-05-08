c4882d5464ae0b90f2fc9636085c7118eff715a7
Mike Boiko <michaelboiko@gmail.com>
2018-05-08 17:27:21 -0600

    Added nicely formatted python argument template

--- templates/skeleton.py
+++ templates/skeleton.py
@@ -7,6 +7,44 @@
 import os        # OS functions
 import sys       # System functions
 
+# Arguments {{{1
+
+# Title
+parser = argparse.ArgumentParser(__doc__)
+
+# Positional arguments
+parser.add_argument(
+        dest='dictFile',
+        action='store',
+        help='Name of dictionary csv list'
+        )
+parser.add_argument(
+        dest='inputFile',
+        action='store',
+        help='Name of original text file, where replacements are to occur'
+        )
+
+# Optional arguments
+parser.add_argument(
+        '-o',
+        '--outputFile',
+        dest='outputFile',
+        action='store',
+        default='output.txt',
+        help='Name of output file (default is output.txt)'
+        )
+
+# Boolean arguments
+parser.add_argument(
+        '--dos',
+        dest='dos_line_ending',
+        action='store_true',
+        help='Specify if this is a dos line ending (default is Unix)'
+        )
+
+# Perform parsing
+args = parser.parse_args()
+
 # Variables {{{1
 
 # Functions {{{1
