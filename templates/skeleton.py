ef24600867595a94fb09128d0c9286bc628cb5de
Mike Boiko <michaelboiko@gmail.com>
2019-05-03 14:30:32 -0600

    Updated python skeleton file

--- templates/skeleton.py
+++ templates/skeleton.py
@@ -1,61 +1,59 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python
 '''
 '''
 # dev: let b:startapp = "./"
 # dev: let b:startfile = @%
-# dev: let b:startargs = ""
+# dev: let b:startargs = "test"
 
 # Imports {{{1
 
-import os        # OS functions
-import sys       # System functions
+import argparse
 
 # Arguments {{{1
 
-# Title
-parser = argparse.ArgumentParser(__doc__)
+# Parse CLI arguments
+parser = argparse.ArgumentParser(
+    description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 # Positional arguments
-parser.add_argument(
-        dest='dictFile',
-        action='store',
-        help='Name of dictionary csv list'
-        )
-parser.add_argument(
-        dest='inputFile',
-        action='store',
-        help='Name of original text file, where replacements are to occur'
-        )
+parser.add_argument(dest='dictFile', action='store', help='Name of dictionary csv list')
 
 # Optional arguments
 parser.add_argument(
-        '-o',
-        '--outputFile',
-        dest='outputFile',
-        action='store',
-        default='output.txt',
-        help='Name of output file (default is output.txt)'
-        )
+    '-o',
+    '--outputFile',
+    dest='outputFile',
+    action='store',
+    default='output.txt',
+    help='Name of output file (default is output.txt)')
 
 # Boolean arguments
 parser.add_argument(
-        '--dos',
-        dest='dos_line_ending',
-        action='store_true',
-        help='Specify if this is a dos line ending (default is Unix)'
-        )
-
-# Perform parsing
-args = parser.parse_args()
+    '--dos',
+    dest='dos_line_ending',
+    action='store_true',
+    help='Specify if this is a dos line ending (default is Unix)')
 
 # Variables {{{1
 
+class MyClass():  # {{{1
+    '''
+    '''
+
+    def __init__(self, **kwargs):  # {{{2
+        '''
+        Initialize all the variables
+        '''
+        pass
+
 # Functions {{{1
-def main(): # {{{2
+def main():  # {{{2
     'Main script entry point'
+    pass
 
 # Main {{{1
 
 # Run script if this file is executed directly
 if __name__ == '__main__':
+    args = parser.parse_args()
     main()
