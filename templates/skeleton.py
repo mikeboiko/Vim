021f6f4f4ca73a85074eb064c8eba4e1af208ddd
Mike Boiko <michaelboiko@gmail.com>
2019-11-11 20:42:24 -0700

    Moved TODOs from .vimrc into jira

--- templates/skeleton.py
+++ templates/skeleton.py
@@ -5,27 +5,25 @@
 # dev: let b:startfile = @%
 # dev: let b:startargs = "test"
 
-# Imports {{{1
-
 import argparse
-
-# Arguments {{{1
 
 # Parse CLI arguments
 parser = argparse.ArgumentParser(
-    description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    description=__doc__,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 # Positional arguments
-parser.add_argument(dest='dictFile', action='store', help='Name of dictionary csv list')
+parser.add_argument(dest='dictFile',
+                    action='store',
+                    help='Name of dictionary csv list')
 
 # Optional arguments
-parser.add_argument(
-    '-o',
-    '--outputFile',
-    dest='outputFile',
-    action='store',
-    default='output.txt',
-    help='Name of output file (default is output.txt)')
+parser.add_argument('-o',
+                    '--outputFile',
+                    dest='outputFile',
+                    action='store',
+                    default='output.txt',
+                    help='Name of output file (default is output.txt)')
 
 # Boolean arguments
 parser.add_argument(
@@ -34,24 +32,21 @@ parser.add_argument(
     action='store_true',
     help='Specify if this is a dos line ending (default is Unix)')
 
-# Variables {{{1
 
-class MyClass():  # {{{1
+class MyClass():
     '''
     '''
-
-    def __init__(self, **kwargs):  # {{{2
+    def __init__(self, **kwargs):
         '''
         Initialize all the variables
         '''
         pass
 
-# Functions {{{1
-def main():  # {{{2
+
+def main():
     'Main script entry point'
     pass
 
-# Main {{{1
 
 # Run script if this file is executed directly
 if __name__ == '__main__':
