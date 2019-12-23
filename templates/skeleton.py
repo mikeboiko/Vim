e02d074e34250852808b2630ce5d5d82e8c14b2f
Mike Boiko <michaelboiko@gmail.com>
2019-12-23 10:55:35 -0700

    Updated skeleton files

--- templates/skeleton.py
+++ templates/skeleton.py
@@ -5,33 +5,6 @@
 # dev: let b:startfile = @%
 # dev: let b:startargs = "test"
 
-import argparse
-
-# Parse CLI arguments
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-# Positional arguments
-parser.add_argument(dest='dictFile',
-                    action='store',
-                    help='Name of dictionary csv list')
-
-# Optional arguments
-parser.add_argument('-o',
-                    '--outputFile',
-                    dest='outputFile',
-                    action='store',
-                    default='output.txt',
-                    help='Name of output file (default is output.txt)')
-
-# Boolean arguments
-parser.add_argument(
-    '--dos',
-    dest='dos_line_ending',
-    action='store_true',
-    help='Specify if this is a dos line ending (default is Unix)')
-
 
 class MyClass():
     '''
@@ -50,5 +23,4 @@ def main():
 
 # Run script if this file is executed directly
 if __name__ == '__main__':
-    args = parser.parse_args()
     main()
