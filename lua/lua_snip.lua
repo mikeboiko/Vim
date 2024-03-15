708f626e107e7ed4b2585307775f89a2d9221664
Mike Boiko <mike@boiko.ca>
2024-03-15 05:56:10 -0600

    Fixed luasnip tab expand

--- lua/lua_snip.lua
+++ lua/lua_snip.lua
@@ -6,19 +6,10 @@ ls.add_snippets("all", {
 	s("cb", { t("- [ ] ") }),
 })
 
-vim.keymap.set({ "i" }, "<TAB>", function()
-	ls.expand()
+vim.keymap.set({ "i", "s" }, "<Tab>", function()
+	if ls.expand_or_jumpable() then
+		ls.expand_or_jump()
+	else
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+	end
 end, { silent = true })
-
--- vim.keymap.set({ "i", "s" }, "<C-L>", function()
--- ls.jump(1)
--- end, { silent = true })
--- vim.keymap.set({ "i", "s" }, "<C-J>", function()
--- ls.jump(-1)
--- end, { silent = true })
-
--- vim.keymap.set({ "i", "s" }, "<C-E>", function()
--- if ls.choice_active() then
--- ls.change_choice(1)
--- end
--- end, { silent = true })
