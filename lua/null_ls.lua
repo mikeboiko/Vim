799b92354ef1ab32b5d8ab1013d1a41487717dd4
Mike Boiko <mike@boiko.ca>
2024-07-10 15:41:17 -0600

    feat(null_ls): exclude CP02 rule for sqlfluff
    The sqlfluff linter and formatter for null_ls now excludes the CP02 rule.
    This change affects both the diagnostics and formatting configurations.
    The `--exclude-rules` argument has been added with the value "CP02".

--- lua/null_ls.lua
+++ lua/null_ls.lua
@@ -7,13 +7,13 @@ null_ls.setup({
 		null_ls.builtins.completion.tags,
 		null_ls.builtins.diagnostics.trail_space,
 		null_ls.builtins.diagnostics.sqlfluff.with({
-			extra_args = { "--dialect", "tsql" },
+			extra_args = { "--dialect", "tsql", "--exclude-rules", "CP02" },
 		}),
 		-- null_ls.builtins.diagnostics.vint,
 		null_ls.builtins.formatting.isort,
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.sqlfluff.with({
-			extra_args = { "--dialect", "tsql" },
+			extra_args = { "--dialect", "tsql", "--exclude-rules", "CP02" },
 		}),
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.yapf,
